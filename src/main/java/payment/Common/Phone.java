5a3ddd0b923f982d6bb23b0b6f7d9b8a8e4ec145
Just1lz <exelent.pride@rambler.ru>
2020-09-20 11:37:57 +0700

    Корректировка метода checkPhonePattern, для корректной работы matches при проверке. (учитывая что дженерик класс может принять любой тип - перед началом работы matches привёл входное поле к строке).

--- src/main/java/payment/Common/Phone.java
+++ src/main/java/payment/Common/Phone.java
@@ -2,19 +2,21 @@ package payment.Common;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.ToString;
 
 @Getter
 @AllArgsConstructor
+@ToString
 
 public class Phone <X>{
-    private String phone;
+    private X phone;
 
     public Phone<X> checkPhonePattern () {
-        phone.replace(" ", "");
-        if (!phone.matches("^(8|\\+7)(([\\- ]?\\(?\\d{3}\\)?[\\- ]?)?(\\d{3}[\\- ]?)?(\\d{2}[\\- ]?)?\\d{2}$)?")) {
+        String rsl = phone.toString();
+        rsl.replace(" ", "");
+        if (!rsl.matches("^(8|\\+7)(([\\- ]?\\(?\\d{3}\\)?[\\- ]?)?(\\d{3}[\\- ]?)?(\\d{2}[\\- ]?)?\\d{2}$)?")) {
             throw new NumberFormatException();
         }
         return this;
     }
-
 }
